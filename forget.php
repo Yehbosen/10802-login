e27f3ab2e62d3105ee2121f1eda871d8f7527238
mack <macklun@ms7.hinet.net>
2019-10-23 23:31:43 +0800

    login template

---
+++ forget.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="#" type="image/x-icon">
+  <title>尋回密碼</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+</body>
+</html>
